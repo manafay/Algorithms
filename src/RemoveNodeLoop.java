864dce4fa162bb6489fedffa8ef3b45af1de23e0
Nafay <manafay@gmail.com>
2019-04-02 00:14:11 +0530

    Detect and remove loop without using extra memory

--- src/RemoveNodeLoop.java
+++ src/RemoveNodeLoop.java
@@ -2,6 +2,17 @@ import java.util.IdentityHashMap;
 
 public class RemoveNodeLoop {
 
+    public static class NodeVisited {
+        int item;
+        int visit;
+        NodeVisited next;
+
+        public NodeVisited() {
+            visit = 0;
+        }
+    }
+
+    // with using extra memory
     public Node detectAndRemoveLoop(Node source) {
         Node head = source;
         IdentityHashMap<Node, Integer> visited = new IdentityHashMap<>();
@@ -17,7 +28,32 @@ public class RemoveNodeLoop {
         return head;
     }
 
+
+    // without using extra memory
+    public NodeVisited removeAndDetectLoop(NodeVisited source) {
+        NodeVisited head = source;
+        NodeVisited temp = source;
+        while(source != null) {
+            if (source.visit > 1) {
+                temp.next = null;
+            } else {
+                source.visit += 1;
+                temp = source;
+            }
+            source = source.next;
+        }
+        return head;
+    }
+
     public void printAll(Node source) {
+        while(source != null) {
+            System.out.print(source.item);
+            System.out.println("*******");
+            source = source.next;
+        }
+    }
+
+    public void printAllVertices(NodeVisited source) {
         while(source != null) {
             System.out.print(source.item);
             System.out.println("*******");
@@ -49,7 +85,29 @@ public class RemoveNodeLoop {
         fourth.next = fifth;
         fifth.next = second;
 
-        removeLoop.printAll(removeLoop.detectAndRemoveLoop(first));
+        NodeVisited firstv = new NodeVisited();
+        firstv.item = 1;
 
+        NodeVisited secondv = new NodeVisited();
+        secondv.item = 2;
+
+        NodeVisited thirdv = new NodeVisited();
+        thirdv.item = 3;
+
+        NodeVisited fourthv = new NodeVisited();
+        fourthv.item = 4;
+
+        NodeVisited fifthv = new NodeVisited();
+        fifthv.item = 5;
+
+        firstv.next = secondv;
+        secondv.next = thirdv;
+        thirdv.next = fourthv;
+        fourthv.next = fifthv;
+        fifthv.next = secondv;
+        System.out.println("With using Extra memory. That is, Identity hash map");
+        removeLoop.printAll(removeLoop.detectAndRemoveLoop(first));
+        System.out.println("Without using Extra memory. That is, by adding a visit parameter in the node class");
+        removeLoop.printAllVertices(removeLoop.removeAndDetectLoop(firstv));
     }
 }
