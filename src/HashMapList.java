a9d51d4323651e1a554ca192c6d070bd3921fb47
Nafay <manafay@gmail.com>
2019-05-03 15:09:51 +0530

    Iterator for Hash Map

--- src/HashMapList.java
+++ src/HashMapList.java
@@ -1,12 +1,42 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Random;
 
 class HashMapList<T, U> {
     private ArrayList<ArrayList<Pair<T, U>>> hashList = new  ArrayList<>(Collections.nCopies(100,null));
 
     private int getHash(T k) {
         return k.toString().length() % 100;
+    }
+
+    public class Iterator<T, U> {
+        private ArrayList<Pair<T, U>> iterList = new ArrayList<>();
+        private int iter;
+
+        private ArrayList randomHead() {
+            // how do I check if the hashMap is not null or at least there is one insertion in place
+            Random rand = new Random();
+            int pick = rand.nextInt(100);
+            return hashList.get(pick) != null ? hashList.get(4) : randomHead();
+        }
+
+        Iterator() {
+           iterList = randomHead();
+           iter = -1;
+        }
+
+        public boolean hasNext() {
+            int nextIndex = iter + 1;
+            if (iterList.size() > nextIndex) {
+                return iterList.get(nextIndex) != null;
+            }
+            return false;
+        }
+
+        public U next() {
+            return iterList.get(++iter).getValue();
+        }
     }
 
     public U get(T k) {
@@ -55,28 +85,35 @@ class HashMapList<T, U> {
         test.put("Azhar", 25);
         test.put("Fatima", 13);
         test.put("Ammi", 5);
+        HashMapList<String, Integer>.Iterator<String, Integer> firstIter = test.new Iterator<String, Integer>();
+        while(firstIter.hasNext()) {
+            System.out.print("********");
+            System.out.println(firstIter.next());
+            System.out.print("********");
+        }
         System.out.println(test.get("Nafay"));
         System.out.println(test.get("Azhar"));
         test.remove("Azhar");
         System.out.println(test.get("Nafay"));
 
-        HashMapList<String, String> test2 = new HashMapList();
-        test2.put("Nafay", "Soccer");
-        test2.put("Azhar", "Cricket");
-        System.out.println(test2.get("Nafay"));
-        System.out.println(test2.get("Azhar"));
-        test2.remove("Azhar");
-        System.out.println(test2.get("Nafay"));
-        System.out.println(test2.get("Azhar"));
 
-        HashMapList<Integer, Boolean> test3 = new HashMapList();
-        test3.put(14, true);
-        test3.put(25, false);
-        System.out.println(test3.get(14));
-        System.out.println(test3.get(25));
-        test3.remove(14);
-        System.out.println(test3.get(14));
-        System.out.println(test3.get(25));
+//        HashMapList<String, String> test2 = new HashMapList();
+//        test2.put("Nafay", "Soccer");
+//        test2.put("Azhar", "Cricket");
+//        System.out.println(test2.get("Nafay"));
+//        System.out.println(test2.get("Azhar"));
+//        test2.remove("Azhar");
+//        System.out.println(test2.get("Nafay"));
+//        System.out.println(test2.get("Azhar"));
+//
+//        HashMapList<Integer, Boolean> test3 = new HashMapList();
+//        test3.put(14, true);
+//        test3.put(25, false);
+//        System.out.println(test3.get(14));
+//        System.out.println(test3.get(25));
+//        test3.remove(14);
+//        System.out.println(test3.get(14));
+//        System.out.println(test3.get(25));
     }
 
 
